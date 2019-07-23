506fad1ae14ac3f1051043cd198025dfa73cb1f7
nemoisme <nemo_ambitioner@163.com>
2019-07-23 23:35:17 +0800

    修改config文件--ps-出入口文件有待目录调整

--- webpack.config.js
+++ webpack.config.js
@@ -1,79 +1,87 @@
-var path = require('path')
-var webpack = require('webpack')
+var path = require("path");
+var webpack = require("webpack");
 
 module.exports = {
-  entry: './src/lib/index.js',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: 'el-validate-table.js',
-    library:'ElValidateTable',
-    libraryTarget: "umd",
-    umdNamedDefine: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          'css-loader'
-        ],
-      },
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {
-          }
-          // other vue-loader options go here
-        }
-      },
-      {
-        test: /\.sass$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          'sass-loader?indentedSyntax'
-        ],
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]?[hash]'
-        }
-      }
-    ]
-  },
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js'
+  dev: {
+    entry: path.resolve(__dirname, "/examples/main.js"),
+    mode: "development",
+    port: 8090,
+    output: {
+      path: path.resolve(__dirname, "/examples/dist/demo-dist.js"),
+      filename: "demo-dist.js"
     },
-    extensions: ['*', '.js', '.vue', '.json']
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src")
+      }
+    }
   },
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true,
-    overlay: true
+  build: {
+    entry: "./src/lib/index.js",
+    output: {
+      path: path.resolve(__dirname, "./dist"),
+      publicPath: "/dist/",
+      filename: "el-validate-table.js",
+      library: "ElValidateTable",
+      libraryTarget: "umd",
+      umdNamedDefine: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ["vue-style-loader", "css-loader"]
+        },
+        {
+          test: /\.vue$/,
+          loader: "vue-loader",
+          options: {
+            loaders: {}
+            // other vue-loader options go here
+          }
+        },
+        {
+          test: /\.sass$/,
+          use: ["vue-style-loader", "css-loader", "sass-loader?indentedSyntax"]
+        },
+        {
+          test: /\.js$/,
+          loader: "babel-loader",
+          exclude: /node_modules/
+        },
+        {
+          test: /\.(png|jpg|gif|svg)$/,
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]?[hash]"
+          }
+        }
+      ]
+    },
+    resolve: {
+      alias: {
+        vue$: "vue/dist/vue.esm.js"
+      },
+      extensions: ["*", ".js", ".vue", ".json"]
+    },
+    devServer: {
+      historyApiFallback: true,
+      noInfo: true,
+      overlay: true
+    },
+    performance: {
+      hints: false
+    },
+    devtool: "#eval-source-map"
   },
-  performance: {
-    hints: false
-  },
-  devtool: '#eval-source-map'
-}
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([
+};
+
+if (process.env.NODE_ENV === "production") {
+  config.devtool = "#source-map";
+  config.plugins = (config.plugins  || []).concat([
     new webpack.DefinePlugin({
-      'process.env': {
+      "process.env": {
         NODE_ENV: '"production"'
       }
     }),
@@ -85,6 +93,17 @@ if (process.env.NODE_ENV === 'production') {
     }),
     new webpack.LoaderOptionsPlugin({
       minimize: true
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      sourceMap: false,
+      mangle: true,
+      compress: {
+        warnings: false
+      }
     })
-  ])
+    
+  ]);
 }
+
+module.exports = config;
