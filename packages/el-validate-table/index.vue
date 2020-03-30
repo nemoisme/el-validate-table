6c2e46bbbb117c43b531ece5c467db63c42f6504
nemo <nemo_ambitioner@163.com>
2020-03-30 16:23:41 +0800

    fix:更新校验交互

--- packages/el-validate-table/index.vue
+++ packages/el-validate-table/index.vue
@@ -1,161 +1,176 @@
 <script>
-import {get, set} from 'lodash'
-import FormLib from 'element-ui/lib/form'
-import TableLib from 'element-ui/lib/table'
-import mixinOptionExtensions from './shared/mixin-package-option.js'
-import {toCamelCase, VALIDATE_FIELD} from './shared/utils.js'
-import {TABLE_FORM_MIX} from './shared/constant.js'
-import {Message,Table,TableColumn,Form,FormItem} from 'element-ui'
-let len = 0 // 全局变量  更正列索引
+import Form from "element-ui/lib/form";
+import Table from "element-ui/lib/table";
+
+import { get, set } from "lodash";
+import mixinOptionExtensions from "./mixin-package-option.js";
+let len = 0; // 全局变量  更正列索引
+
+/**
+ * 转换为大小驼峰命名
+ * abc-efg => abcEfg
+ */
+export const toCamelCase = str => {
+  return str.indexOf("-") !== -1
+    ? str.replace(/-([a-zA-Z])/g, ($0, $1) => $1.toUpperCase())
+    : str;
+};
 
 export default {
-  name: 'el-validate-table',
-  mixins: [TABLE_FORM_MIX, mixinOptionExtensions],
+  name: "el-validate-table",
   components: {
-    'el-table':Table,
-    'el-table-column':TableColumn,
-    'el-form':Form,
-    'el-form-item':FormItem
+    // "validate-column": () => import("./validate-column")
   },
-  props: Object.assign({}, FormLib.props, TableLib.props, {
+  mixins: [mixinOptionExtensions],
+  props: {
     data: Array,
-    columns: Array
-  }),
+    columns: Array,
+    isDrag: Boolean
+  },
   data() {
     return {
       spanObject: {},
       customSpanMethod: () => {}
-    }
+    };
   },
   watch: {
     data: {
-      handler: 'rowspan',
+      handler: "rowspan",
       immediate: true
     },
     columns: {
-      handler: 'rowspan',
+      handler: "rowspan",
       immediate: true
     }
   },
   render(h) {
     const slots = Object.keys(this.$slots)
       .reduce((arr, key) => {
-        arr = arr.concat(this.$slots[key])
-        return arr
+        arr = arr.concat(this.$slots[key]);
+        return arr;
       }, [])
       .map(vnode => {
-        vnode.context = this._self
-        return vnode
-      })
+        vnode.context = this._self;
+        return vnode;
+      });
 
-    return (
-      <el-form
-        ref="elForm"
-        {...{
-          props: {model: {data: this.data}, ...this.attrs},
-          on: {
-            ...this.$listeners,
-            validate: (prop, valid, msg) => {
-              if (!valid) {
-                this.$message.warning(msg)
-              }
-            }
+    return h(
+      "el-form",
+      {
+        props: {
+          model: {
+            data: this.data
           }
-        }}
-      >
-        <el-table
-          ref="elTable"
-          {...{
-            props: {
+        },
+        ref: "elForm"
+      },
+      [
+        h(
+          "el-table",
+          {
+            props: Object.assign({}, this.$attrs, {
               data: this.data,
-              'span-method': this.customSpanMethod,
-              ...this.$attrs
-            },
-            on: this.$listeners
-          }}
-        >
-          {this.renderColumns(h, this.columns).concat(slots)}
-        </el-table>
-      </el-form>
-    )
+              "span-method": this.customSpanMethod
+            }),
+            on: { ...this.$listeners },
+            ref: "elTable"
+          },
+          this.renderColumns(h, this.columns).concat(slots)
+        )
+      ]
+    );
   },
   async mounted() {
-    this.customSpanMethod =
-      this.$attrs['custom-merge'] ||
-      this.$attrs['span-method'] ||
-      this.$attrs.spanMethod ||
-      this.arraySpanMethod
-    await this.$nextTick(() => {})
+    const columnsMapByprop = this.columns.reduce((cur, col) => {
+      cur[col.prop] = col;
+      return cur;
+    }, {});
+    this.customSpanMethod = ({ row, column, rowIndex, columnIndex }) => {
+      const curCol = columnsMapByprop[column.property];
+      const mergeFunc =
+        this.$attrs["span-method"] ||
+        this.$attrs.spanMethod ||
+        this.arraySpanMethod;
+      return mergeFunc({ row, column, rowIndex, columnIndex });
+    };
+
+    await this.$nextTick(() => {});
 
     // 同步form,table中的mehods
-    const methods = Object.assign({}, Form.methods, Table.methods)
+    const methods = Object.assign({}, Form.methods, Table.methods);
     Object.keys(methods).forEach(key => {
       if (this.$refs.elForm.hasOwnProperty(key)) {
-        this[key] = this.$refs.elForm[key]
+        this[key] = this.$refs.elForm[key];
       } else {
-        this[key] = this.$refs.elTable[key]
+        this[key] = this.$refs.elTable[key];
       }
-    })
+    });
   },
   methods: {
     rowspan() {
-      const data = this.data
+      const data = this.data;
       const getSpanObject = (arr, con = {}) => {
         return arr.reduce((sum, col) => {
           if (col.children && col.children.length) {
-            getSpanObject(col.children, sum)
-            return sum
+            getSpanObject(col.children, sum);
+            return sum;
           }
 
-          if (!col.isMerge) {
-            return sum
+          if (!col.colMerge) {
+            return sum;
           }
 
-          sum[col.prop] = []
+          sum[col.prop] = [];
 
-          let position
-          let spanArr = sum[col.prop]
+          let position;
+          let spanArr = sum[col.prop];
 
           Array.isArray(data) &&
             data.forEach((item, index) => {
               if (index === 0) {
-                spanArr.push(1)
-                position = 0
+                spanArr.push(1);
+                position = 0;
               } else {
                 if (data[index][col.prop] === data[index - 1][col.prop]) {
-                  spanArr[position] += 1
-                  spanArr.push(0)
+                  spanArr[position] += 1;
+                  spanArr.push(0);
                 } else {
-                  spanArr.push(1)
-                  position = index
+                  spanArr.push(1);
+                  position = index;
                 }
               }
-            })
-          return sum
-        }, con)
-      }
+            });
+          return sum;
+        }, con);
+      };
 
-      this.spanObject = getSpanObject(this.columns)
+      this.spanObject = getSpanObject(this.columns);
     },
-    arraySpanMethod({row, column, rowIndex, columnIndex}) {
-      const spanArr = this.spanObject[column.property]
+    arraySpanMethod({ row, column, rowIndex, columnIndex }) {
+      const spanArr = this.spanObject[column.property];
       if (spanArr) {
-        const _row = spanArr[rowIndex]
-        const _col = _row > 0 ? 1 : 0
+        const _row = spanArr[rowIndex];
+        const _col = _row > 0 ? 1 : 0;
         return {
           rowspan: _row,
           colspan: _col
-        }
+        };
       }
+      // if (rowIndex % 2 === 0) {
+      //   if (columnIndex === 1) {
+      //     return [3, 2];
+      //   }
+      // }
     },
+    rowColMerge({ row, column, rowIndex, columnIndex }) {},
     renderColumns(h, columns) {
       return (
         columns &&
         columns.map((col, index) => {
-          let columnIndex
-          len = col.children ? len : len + 1
-          columnIndex = len - 1
-
+          let columnIndex;
+          len = col.children ? len : len + 1;
+          columnIndex = len - 1;
+          // console.log(col, this.columns, '对比1')
           const scopedSlots = {
             default: scope => {
               // console.log(scope, 'scope')
@@ -165,7 +180,7 @@ export default {
                 columnIndex,
                 prop: col.prop,
                 column: scope.column
-              }
+              };
 
               const {
                 type = null,
@@ -177,26 +192,27 @@ export default {
                 component,
                 props
               } =
-                typeof col.config == 'function'
+                typeof col.config == "function"
                   ? !!col.config(params) && col.config(params)
-                  : {}
+                  : {};
 
               const ele =
-                type || (typeof component == 'object' && component) || null
-
-              const isFormItem = Array.isArray(rules) && rules.length > 0
+                (type && "el-" + type) ||
+                (typeof component == "object" && component) ||
+                null;
+              const isFormItem = Array.isArray(rules) && rules.length > 0;
 
               return !!ele
                 ? h(
-                    isFormItem ? 'el-form-item' : 'div',
+                    isFormItem ? "el-form-item" : "div",
                     {
                       props: {
                         prop: `data[${scope.$index}].${col.prop}`,
                         rules
-                      },
-                      style: {
-                        marginBottom: 0
                       }
+                      // style: {
+                      //   marginBottom: 0
+                      // }
                     },
                     [
                       h(
@@ -214,10 +230,10 @@ export default {
                               input: val => {
                                 get(scope.row, col.prop) === undefined
                                   ? this.$set(scope.row, col.prop, val)
-                                  : set(scope.row, col.prop, val)
+                                  : set(scope.row, col.prop, val);
                               },
                               change: val => {
-                                this.$emit('cell-change', params)
+                                this.$emit("cell-change", params);
                               }
                             },
                             event
@@ -235,30 +251,61 @@ export default {
                       get(scope.row, col.prop),
                       scope.$index
                     )) ||
-                    (typeof col.render == 'function' &&
+                    (typeof col.render == "function" &&
                       col.render(h, params)) ||
-                    get(scope.row, col.prop)
+                    get(scope.row, col.prop);
             }
-          }
+          };
           return h(
-            'el-table-column',
+            "el-table-column",
             {
               props: Object.assign({}, this.$attrs, col),
               key: index,
-              scopedSlots
+              scopedSlots:
+                col.type && col.type == "selection" ? null : scopedSlots
             },
             this.renderColumns(h, col.children)
-          )
+          );
         })
-      )
+      );
     },
     renderOps(type, options) {
-      const optType = type && type.indexOf('el-') === 0 ? type.slice(3) : type
-      let optRenderer = optType && this[`${toCamelCase(optType)}_opt`]
-      if (typeof optRenderer === 'function' && Array.isArray(options)) {
-        return options.map(optRenderer)
+      const optType = type && type.indexOf("el-") === 0 ? type.slice(3) : type;
+      let optRenderer = optType && this[`${toCamelCase(optType)}_opt`];
+      if (typeof optRenderer === "function" && Array.isArray(options)) {
+        return options.map(optRenderer);
       }
     }
   }
-}
+};
 </script>
+<style scoped>
+.el-form-item {
+  margin-bottom: 0;
+}
+.is-error {
+  animation: errvalid 0.5s;
+  animation-fill-mode: forwards;
+}
+@keyframes errvalid {
+  from {
+    margin-bottom: 0;
+  }
+  to {
+    margin-bottom: 22px;
+  }
+}
+.is-success {
+  animation: sucvalid 0.5s;
+  animation-fill-mode: forwards;
+}
+
+@keyframes sucvalid {
+  from {
+    margin-bottom: 22px;
+  }
+  to {
+    margin-bottom: 0;
+  }
+}
+</style>
