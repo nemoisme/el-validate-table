c1af1d22a635cadf5681a30ca66f7204a9d6d9f9
nemo <nemo_ambitioner@163.com>
2020-03-30 14:47:31 +0800

    update:更新配置

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <el-validate-table :data="data" :columns="columns" />
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "App",
+  components: {
+  },
+  data() {
+    return {
+      data:[
+        {
+          a:1,
+          b:2
+        }
+      ],
+      columns:[
+        {
+          label:'测试1',
+          prop:'a'
+        },
+        {
+          label:'测试2',
+          prop:'b'
+        }
+      ]
+    }
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
